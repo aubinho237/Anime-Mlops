02eb8ad43169fb5534d31b9791988d5225bad917
HA Bao Ngoc <76453906+hafbn@users.noreply.github.com>
2023-02-24 17:07:48 +0100

    Update test pour App.js

--- myfrontendapp/src/App.test.js
+++ myfrontendapp/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Form from './Form';
+
+test('renders the form', () => {
+  render(
+    <MemoryRouter>
+      <Form />
+    </MemoryRouter>
+  );
+  const formElement = screen.getByText('Submit');
+  expect(formElement).toBeInTheDocument();
 });
